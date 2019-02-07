86ec81ce9c6bf91ab03abd93bbcfda17953745f7
visualstudio <visualstudio@230-PC29>
2019-02-07 11:57:53 -0500

    I COMMIT

--- week-5(2)/js/main.js
+++ week-5(2)/js/main.js
@@ -5,6 +5,12 @@
 function arrays() {
 
     var pens;
-    pens = ["red", "orange", "green", "blue", "purple"];
-    console.log(pens);
+    pens = new Array("red", "orange", "green", "blue", "purple");
+    markers = pens;
+    console.log(markers);
+    console.log(markers[0]);
+    for (i = 0; i < markers.length; i++) {
+        alert(markers[i]);
+    }
+    alert("length of array is:" + markers.length);
 }
