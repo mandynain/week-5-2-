e6d163f03a8b4d481f90b340b54667113e5357fe
visualstudio <visualstudio@230-PC29>
2019-02-07 11:46:10 -0500

    i commit

--- week-5(2)/js/main.js
+++ week-5(2)/js/main.js
@@ -1,1 +1,10 @@
-﻿// Your code here!
+﻿function myFunction(a, b) {
+   
+    return a + b; //the function returns the product of p1 and p2
+}
+function arrays() {
+
+    var pens;
+    pens = ["red", "orange", "green", "blue", "purple"];
+    console.log(pens);
+}
